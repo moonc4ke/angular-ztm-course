7c3d0979b24a77815f0f156318f6fed8468c3cf2
Donatas <adom.donatas@gmail.com>
2022-07-14 12:07:24 +0300

    more hooks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,13 +1,47 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
+export class PostComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
   constructor() {
     console.log('constructor', this.postImg);
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
   }
 
   @Input('img') postImg = '';
@@ -15,6 +49,18 @@ export class PostComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('ngOnInit', this.postImg);
+  }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges called');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
     
   }
 }
