16a6d45ba99d6be18868426a264d74c4cf936fe2
Donatas <adom.donatas@gmail.com>
2022-07-14 11:51:10 +0300

    hooks

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  constructor() {
+    console.log('constructor', this.postImg);
+  }
+
   @Input('img') postImg = '';
   @Output() imgSelected = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    console.log('ngOnInit', this.postImg);
+    
+  }
 }
