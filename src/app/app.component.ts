d201bb72f1927857ba24e964eb5aa3c8bad09154
Donatas <adom.donatas@gmail.com>
2022-07-19 10:51:21 +0300

    practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Luis';
+  name = 'luis ramirez';
   imgUrl =
     'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+  images = [
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large',
+  };
+  blueClass = false;
+  fontSize = 16;
 
   getName() {
     return this.name;
